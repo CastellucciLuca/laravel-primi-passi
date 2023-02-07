96be46bc3b9cdbc0c8b074d8b18d6ca0a934ae5f
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-07 15:48:50 +0100

    Add bonus

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,22 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Contacts</title>
+
 </head>
 <body>
-    <header>
+<header>
         <nav>
             <ul>
                 <li>
                     <a href="/home/">Home</a>
-                    <a href="/work-with-us/">Work with us</a>
+                    <a href="/about-us/">About Us</a>
                     <a href="/contacts/">Contacts</a>
                 </li>
             </ul>
         </nav>
     </header>
     <main>
-        <h1>HOME</h1>
+        <h1>Contacts</h1>
     </main>
 </body>
 </html>
